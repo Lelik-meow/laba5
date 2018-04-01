35bdce242b3889d41f5528618eb102dae3bd9c8a
Yulia <lelik2417@gmail.com>
2018-04-01 03:26:17 +0300

    comm2

--- std.h
+++ std.h
@@ -1,7 +1,10 @@
 #ifndef ADD_H 
 #define ADD_H 
 
-int add() 
+int add(int number1,int number2) 
 { 
-
+cout « "Введите первое число: "; 
+cin » number1; 
+cout « "Введите второе число: "; 
+cin » number2; 
 }
