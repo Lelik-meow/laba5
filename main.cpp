85af2ec72db6461c2c692f3107d3f4f6ced70a80
Yulia <lelik2417@gmail.com>
2018-04-01 04:10:54 +0300

    comm7

--- main.cpp
+++ main.cpp
@@ -5,4 +5,10 @@
 //Программа является учебной.
 int main () 
 { 
+int a, b; 
+cout « "Enter A and B: "; 
+cin » a » b; 
+cout « "A + B = " « a + b « '\n' 
+« "A - B = " « a - b « '\n'
+« "A * B = " « a * b « '\n';
 }
