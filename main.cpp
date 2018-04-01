43dd8d9f05e71a65ac6567d6832c43e29fec4091
Yulia <lelik2417@gmail.com>
2018-04-01 04:43:23 +0300

    comm9

--- main.cpp
+++ main.cpp
@@ -10,5 +10,7 @@ cout « "Enter A and B: ";
 cin » a » b; 
 cout « "A + B = " « a + b « '\n' 
 « "A - B = " « a - b « '\n'
-« "A * B = " « a * b « '\n';
+« "A * B = " « a * b « '\n'
+« "A / B = " « a / b « '\n';
 }
+//Готовая программа.
